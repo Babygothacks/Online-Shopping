5f8082fd265dcd995268f19e1ae0f4ec552abb4e
dell <dell@dell-PC>
2019-05-20 22:55:28 +0530

    Initial project creation

---
+++ shoppingbackend/src/main/java/net/Ganesh/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.Ganesh.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
